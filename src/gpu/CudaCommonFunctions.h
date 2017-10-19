70d8061bb66cad3251d69718d0fe8882c7f7e294
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2017-10-19 12:16:34 +0200

    Add a function to check for cuda errors after a kernel execution call

--- src/gpu/CudaCommonFunctions.h
+++ src/gpu/CudaCommonFunctions.h
@@ -17,3 +17,12 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
         if (abort) exit(code);
     }
 }
+
+#define cudaCheckExecution() { executionAssert(__FILE__, __LINE__); }
+inline void executionAssert(const char *file, int line, bool abort = true) {
+    cudaError_t code = cudaGetLastError();
+    if (code != cudaSuccess) {
+        fprintf(stderr, "CUDA kernel execution failure: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort) exit(code);
+    }
+}
