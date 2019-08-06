1ab13bfe4c56f01cf24d461b7fffbd918aceb01f
Christian Schlinkmann <chsc16@dfki.de>
2019-08-06 13:01:15 +0200

    Fixed unit tests to use CUDA device 0

--- test/src/unit/Main.cpp
+++ test/src/unit/Main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-    _putenv("CUDA_VISIBLE_DEVICES=1");
+    _putenv("CUDA_VISIBLE_DEVICES=0");
     testing::InitGoogleTest(&argc, argv);
     int result = RUN_ALL_TESTS();
 #ifdef _WINDOWS
